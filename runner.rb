49f897fbb031b18a90f5b8caf03604da91a42ad2
Ben Ashwell <ben.ashwell@madetech.com>
2022-07-28 14:30:57 +0100

    Addition of a runner file for testing on CLI

---
+++ runner.rb
@@ -0,0 +1,21 @@
+require 'orders'
+require 'cashier'
+require 'barista'
+
+def main
+  orders = Orders.new
+  cashier = Cashier.new(orders)
+  barista = Barista.new(orders)
+
+  open = true
+  while open
+    puts "What is your name?"
+    name = gets.chomp()
+    cashier.take_customers_order(name)
+    barista.begin_processing_next_order
+    barista.process_order(orders.get_processing_orders[0])
+    barista.complete_current_order
+  end
+end
+
+main
