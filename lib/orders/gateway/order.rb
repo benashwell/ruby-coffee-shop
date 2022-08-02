3cfd9aa11834b2220531e7bca8226bd852383ebc
Ben Ashwell <ben.ashwell@madetech.com>
2022-08-02 11:11:49 +0100

    Add tests for get order gateway method. Add simple cov gem to get coverage stats

--- lib/orders/gateway/order.rb
+++ lib/orders/gateway/order.rb
@@ -3,23 +3,28 @@ require 'orders/domain/order'
 module Orders
   module Gateway
     class Order
-      #Class variable order count
-      @@order_count = 1
 
       def initialize
         @orders = Array.new
+
+        #Order count for ID population
+        @order_count = 1
       end
 
       #Gateway method to create the order - this implementation means that we can create in memory currently but could be out to db or file etc
       def create_order(attributes)
         order = Orders::Domain::Order.new(attributes)
-        order.id = @@order_count
+        order.id = @order_count
         @orders << order
-        @@order_count += 1
+        @order_count += 1
         return order.id
       end
 
       def get_order(order_id)
+        if order_id.nil?
+          raise OrderError
+        end
+
         return @orders.detect { |order| order.id == order_id }
       end
     end
--- spec/orders/gateway/order_spec.rb
+++ spec/orders/gateway/order_spec.rb
@@ -21,4 +21,33 @@ describe Orders::Gateway::Order do
       it { expect {subject }.to raise_error(OrderError) }
     end
   end
+
+  context 'when getting an order' do
+    before(:context) do
+      @under_test = described_class.new
+    end
+
+    subject do
+      @under_test.get_order(order_id)
+    end
+
+    context 'with a nil order id' do
+      let(:order_id) {nil}
+      it { expect{subject}.to raise_error(OrderError) }
+    end
+
+    context 'with an order id that does not exist' do
+      let(:order_id) {5000}
+      it { expect(subject).to be_nil }
+    end
+
+    context 'with an order id that does exist' do
+      let(:order_id) {1}
+      it {
+        @under_test.create_order(customers_name: "Ben")
+        expect(subject).to_not be_nil
+        expect(subject.customers_name).to eq("Ben")
+      }
+    end
+  end
 end
